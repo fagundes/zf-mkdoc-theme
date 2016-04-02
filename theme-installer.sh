a6ea1d26e4a1ad002c365e095de69bf00b6933ca
Vinícius Fagundes <mvlacerda@gmail.com>
2016-04-01 23:02:01 -0300

    generate zff-projects branch

--- theme-installer.sh
+++ theme-installer.sh
@@ -26,7 +26,7 @@ pip install --user pymdown-extensions
 if [[ ! -d "zf-mkdoc-theme/theme" ]];then
     echo "Downloading zf-mkdoc-theme..." ;
     mkdir -p zf-mkdoc-theme ;
-    curl -s -L https://github.com/zendframework/zf-mkdoc-theme/releases/latest | egrep -o '/zendframework/zf-mkdoc-theme/archive/[0-9]*\.[0-9]*\.[0-9]*\.tar\.gz' | head -n1 | wget -O zf-mkdoc-theme.tgz --base=https://github.com/ -i - ;
+    curl -s -L https://github.com/fagundes/zf-mkdoc-theme/releases/latest | egrep -o '/fagundes/zf-mkdoc-theme/archive/[0-9]*\.[0-9]*\.[0-9]*\.tar\.gz' | head -n1 | wget -O zf-mkdoc-theme.tgz --base=https://github.com/ -i - ;
     (
         cd zf-mkdoc-theme ;
         tar xzf ../zf-mkdoc-theme.tgz --strip-components=1 ;
